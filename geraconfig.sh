59d62f6c40c6524bb786f83dd1cae840783070fa
Elthon Freitas <elthonf@gmail.com>
2020-04-12 16:46:33 -0300

    Ajustes para rodar na GCP

--- geraconfig.sh
+++ geraconfig.sh
@@ -8,7 +8,7 @@ echo '      "url": "http://'$(sudo docker inspect serving01 | python3 -c "import
 echo '    },                                                ' >> config/microservices.json
 echo '    "modelo02": {                                     ' >> config/microservices.json
 echo '      "version": "V01",                               ' >> config/microservices.json
-echo '      "url": "http://'$(sudo docker inspect serving01 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
+echo '      "url": "http://'$(sudo docker inspect serving02 | python3 -c "import sys, json; print(json.load(sys.stdin)[0]['NetworkSettings']['Networks']
 ['plat_network']['IPAddress'])")':8080/predict"          ' >> config/microservices.json
 echo '    }                                                 ' >> config/microservices.json
 echo '  }                                                   ' >> config/microservices.json
