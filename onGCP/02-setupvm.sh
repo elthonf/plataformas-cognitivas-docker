50f194bb880014bc1711c6fea13502771381f2a4
Elthon Freitas <elthonf@gmail.com>
2020-06-23 18:46:08 -0300

    Código para criar VM  em GCP

--- onGCP/02-setupvm.sh
+++ onGCP/02-setupvm.sh
@@ -4,6 +4,12 @@
 echo "Install programas uteis"
 sudo apt-get -y update
 sudo apt-get -y install tree
+sudo apt -y install docker.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+
 
 
 echo "Install Anaconda"
